35dd9c8a4da5b1c5ead407153c1e0276242068db
okochi-mika <hiyoko_m@icloud.com>
2026-04-18 16:48:35 +0900

    セトリを更新

--- setlist.js
+++ setlist.js
@@ -1,15 +1,14 @@
 // setlist.js の冒頭
 const setlistData = [
-    { title: "1. all the way ", id: "ZjvQ6Vj_wuw" }, // YouTube IDを調べて入れる
-    { title: "2. それが、愛でしょう", id: "TMoOdbjcjGU" },
-    { title: "3. If ~もしも願いが叶うなら~", id: "ef1JsUS7Qkk" }, 
-    { title: "4. pavement", id: "bxJjiyhpKiw" },
-    { title: "5. し・か・ら・れ・て", id: "UlOYSURhafg" },
-    { title: "6. Remember", id: "iDacFs674Kc" },
-    { title: "7. この場所が好きで", id: "tMR3hb4iR34" },
-    { title: "8. ~HIKARI~", id: "e1CC_UW49dw" },
-    { title: "9. Alone", id: "JjWwfUbOP0w" },
-    { title: "10. 夏花火", id: "KKYMYE1gyNI" },
-    { title: "11. 2000EXPRESS", id: "EHD0Tm-wji0" },
-    { title: "12. Love song on the radio", id: "tHwMV_zfq2A" }
+    { title: "1. キミの願い ", id: "vSEqeEtLMG4" }, // YouTube IDを調べて入れる
+    { title: "2. 君に吹く風", id: "iPkuvrQaJQg" },
+    { title: "3. それが、愛でしょう", id: "TMoOdbjcjGU" }, 
+    { title: "4. キミノユメ", id: "HEHvGgZsdl8" },
+    { title: "5. all the way", id: "ZjvQ6Vj_wuw" },
+    { title: "6. KOHAKU", id: "FZsIi1KH4xI" },
+    { title: "7. Life", id: "0mTnSzDFhyE" },
+    { title: "8. 枯れない花", id: "HKOy3b5ZSVQ" },
+    { title: "9. はじまり", id: "JjWwfUbOP0w" },
+    { title: "10. Alone", id: "KKYMYE1gyNI" },
+    { title: "11. 南風", id: "f8Nv-5wIlbg" }
 ];
